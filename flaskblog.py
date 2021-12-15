cb5896d4ed0172b0f9fe2ab8b3d700c16dcfa209
shashwat2710 <40847974+shashwat2710@users.noreply.github.com>
2021-12-15 20:31:30 +0530

    Basic concept of flask and template update

--- flaskblog.py
+++ flaskblog.py
@@ -1,7 +1,33 @@
-from flask import Flask
+from re import TEMPLATE
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Shashwat Ahuja',
+        'title': 'Blog Post 1',
+        'Content': 'First Post Content',
+        'date_posted': 'December 15 2021'
+    },
+    {
+        'author': 'Sakshi Ahuja',
+        'title': 'Blog Post 2',
+        'Content': 'Second Post Content',
+        'date_posted': 'December 16 2021'
+    }
+]
+
+
+
 @app.route('/')
-def hello_world():
-    return "<p>Hello, World - their! - shashwat</p>"
+@app.route('/home')
+def home():
+    return render_template('home.html',posts=posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html',title='About')
+
+if __name__ == '__main__':
+    app.run(debug=True)
